04c7dbd414fe90dbe5ddb22048fcf125e595e183
Ivan Gasques <ivanngasques@gmail.com>
2022-08-13 18:18:41 +0100

    Header

--- src/components/article/Navbar/Navbar.jsx
+++ src/components/article/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 
-import{RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import{RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+
 import logo from '../../assets/logo.svg'
 
 
 // BEM -> Block Element Modifier
 
 const Navbar = () => {
+
+  const [toggleMenu, setTooggleMenu] = useState(false)
+
+  const Menu = () =>{ 
+  return (<>
+    <p><a href='home'>Home</a> </p>
+    <p><a href='wgt3'>What is GPT-3</a> </p>
+    <p><a href='possibility'>Open AI</a> </p>
+    <p><a href='features'>Case Studies</a> </p>
+    <p><a href='blog'>Library</a> </p>
+    </>)
+  }
   return (
     <div className='gpt3__navbar'>
        <div className='gpt3__navbar-links'>
@@ -15,13 +28,28 @@ const Navbar = () => {
           <img src={logo} alt="logo"/>
     </div>
           <div className='gpt3__navbar-links_container'>
-            <p><a href='home'>Home</a> </p>
-            <p><a href='wgt3'>What is GPT-3</a> </p>
-            <p><a href='possibility'>Open AI</a> </p>
-            <p><a href='features'>Case Studies</a> </p>
-            <p><a href='home'>Library</a> </p>
+            <Menu/>
           </div>
     </div>
+    <div className='gpt3__navbar-sign'>
+      <p>Sign in</p>
+      <button type='button'> Sign up</button>
+    </div>
+    <div className='gpt3__navbar-menu'>
+    {toggleMenu ? 
+    <RiCloseLine color='#fff' size={27} onClick={()=> setTooggleMenu(false)}/> 
+    : 
+    <RiMenu3Line color='#fff' size={27} onClick={()=> setTooggleMenu(true)}/>} 
+
+    {toggleMenu &&
+    (<div className='gpt3__navbar-menu_container scale-up-center'>
+      <div className='gpt3__navbar-menu_container-links'><Menu/>
+      <div className='gpt3__navbar-menu_container-links-sign'>
+      <p>Sign in</p>
+      <button type='button'> Sign up</button>
+    </div></div>
+    </div>)
+    }</div>
     </div>
   )
 }
